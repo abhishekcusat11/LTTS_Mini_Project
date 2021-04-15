65adf23ed5abf3328ac6882d48b305797d6979e6
abhishekcusat11 <48598232+abhishekcusat11@users.noreply.github.com>
2021-04-15 11:53:41 +0530

    Add files via upload

---
+++ 3_Implementation/test/AirTesting.c
@@ -0,0 +1,126 @@
+#include "unity.h"
+#include "AirHeader.h"
+
+/**
+ * @brief Defining Project_main
+ * 
+ */
+#define PROJECT_NAME    "air_Ticket"
+// ruct a new run test object (Run test cases)
+//  * 
+//  */
+//   RUN_TEST(test_DisplayAirline);
+//   RUN_TEST(test_Display_Passenger);
+//   RUN_TEST(test_Reservation_Air);
+//   RUN_TEST(test_DisplayAirlineDetails);
+//   RUN_TEST(test_DisplayPassengerDetails);
+
+//   /**
+//    * @brief Unity framwork Ended
+//    * 
+//    * @return return 
+//    */
+//   return UNITY_END();
+// }
+
+// /**
+//  * @brief Function 
+//  * 
+//  */
+// void test_DisplayAirline(void) {
+  
+
+// }
+// void test_Display_Passenger(void) {
+ 
+  
+// }
+// void test_Reservation_Air(void) {
+ 
+  
+// }
+
+// void test_DisplayAirlineDetails(void) {
+  
+  
+// }
+
+// void test_DisplayPassengerDetails(void) {
+  
+ 
+// }
+
+/**
+ * @brief Function Prototyping for testing
+ * 
+ */
+void test_DisplayAirline(void);
+void test_Display_Passenger(void);
+void test_Reservation_Air(void);
+void test_DisplayAirlineDetails(void);
+void test_DisplayPassengerDetails(void);
+
+/**
+ * @brief Set the Up object /Used for unity framwork "Needed"
+ * 
+ */
+void setUp(){}
+void tearDown(){}
+
+/**
+ * @brief Start of testing from here
+ * 
+ * @return int 
+ */
+int main()
+{
+/**
+ * @brief Construct a new unity begin object
+ * 
+ */
+  UNITY_BEGIN();
+
+/**
+ * @brief Construct a new run test object (Run test cases)
+ * 
+ */
+  RUN_TEST(test_DisplayAirline);
+  RUN_TEST(test_Display_Passenger);
+  RUN_TEST(test_Reservation_Air);
+  RUN_TEST(test_DisplayAirlineDetails);
+  RUN_TEST(test_DisplayPassengerDetails);
+
+  /**
+   * @brief Unity framwork Ended
+   * 
+   * @return return 
+   */
+  return UNITY_END();
+}
+
+/**
+ * @brief Function 
+ * 
+ */
+void test_DisplayAirline(void) {
+  
+
+}
+void test_Display_Passenger(void) {
+ 
+  
+}
+void test_Reservation_Air(void) {
+ 
+  
+}
+
+void test_DisplayAirlineDetails(void) {
+  
+  
+}
+
+void test_DisplayPassengerDetails(void) {
+  
+ 
+}
